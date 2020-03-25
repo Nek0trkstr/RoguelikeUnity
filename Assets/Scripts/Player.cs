771a3afccdd620c451cf475f1471dc0f32f0d344
Michael Shebeko <michael.shebeko@hcl.com>
2020-03-25 23:23:47 +0200

    Init top down rogue like

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float m_MoovingSpeed = 2f;
+    public Rigidbody2D m_RigidBody;
+    private Vector2 m_MoveDirection; 
+ 
+    private void FixedUpdate()
+    {
+        m_MoveDirection.y = Input.GetAxisRaw("Vertical");
+        m_MoveDirection.x = Input.GetAxis("Horizontal");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        m_RigidBody.MovePosition(m_RigidBody.position + m_MoveDirection * m_MoovingSpeed * Time.fixedDeltaTime);
+    }
+}
