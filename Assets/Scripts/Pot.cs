d479339a47e27029903e5521669245dc029ccabe
Michael Shebeko <michael.shebeko@hcl.com>
2020-03-28 18:16:20 +0300

    Add breakable objects

---
+++ Assets/Scripts/Pot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pot : MonoBehaviour, IBreakable
+{
+    private int m_Health = 1;
+    private bool m_IsDestroyed = false;
+    private Animator m_Animator;
+
+    void Awake()
+    {
+        m_Animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ReceiveDamage(int i_ReceivedDamage)
+    {
+        Debug.Log("Received dmg!");
+        m_Health = m_Health - i_ReceivedDamage;
+        if(m_Health <= 0)
+        {
+            m_Animator.SetBool("IsDestroyed", true);
+        }
+    }
+
+    public void DestroySelf()
+    {
+        m_IsDestroyed = true;
+    }
+}
