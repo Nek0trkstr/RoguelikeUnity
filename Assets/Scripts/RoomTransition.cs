343fc882ffd6edb3189e27797cf08c0287188f19
Michael Shebeko <michael.shebeko@hcl.com>
2020-03-31 23:08:01 +0300

    Add room transition

---
+++ Assets/Scripts/RoomTransition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTransition : MonoBehaviour
+{
+    public CameraMovement m_CameraPosition;
+    public Room m_NextRoom;
+
+    void OnTriggerEnter2D(Collider2D i_Other)
+    {
+
+        if (i_Other.CompareTag("Player"))
+        {
+            m_CameraPosition.m_MaxPosition = m_NextRoom.m_MaxCameraPosition;
+            m_CameraPosition.m_MinPosition = m_NextRoom.m_MinCameraPosition;
+        }
+    }
+}
