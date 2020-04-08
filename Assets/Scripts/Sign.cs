4138f452f2880aeba68ab98e7c007b344d81762a
Michael <mshebeko@gmail.com>
2020-04-08 23:31:48 +0300

    Add signs (#18)
    Co-authored-by: Michael Shebeko <michael.shebeko@hcl.com>

--- Assets/Scripts/Sign.cs
+++ Assets/Scripts/Sign.cs
@@ -1,14 +1,70 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class Sign : MonoBehaviour, IInteractive
-{
-    public string m_DialogToShow;
-    public Text m_DialogText;
+using UnityEngine.InputSystem;
 
-    public void Interact()
+public class Sign : MonoBehaviour
+{
+    public GameObject m_DialogToShow;
+    public Text m_DialogText;
+    public string m_DialogContext;
+    public bool m_IsPlayerInRange;
+    private PlayerInputActions m_InputActions;
+    private double m_LastActionTriggerTime = 0;
+    
+    private void Awake()
     {
+        m_InputActions = new PlayerInputActions();
+        m_InputActions.PlayerControls.Attack.performed += OnPlayerAction;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            m_IsPlayerInRange = true;
+        }
         
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            m_IsPlayerInRange = false;
+            if (m_DialogToShow.activeInHierarchy)
+            {
+                m_DialogToShow.SetActive(false);
+            }
+        }
+    }
+
+    private void OnPlayerAction(InputAction.CallbackContext i_Ctx)
+    {
+        if (m_IsPlayerInRange == true && (i_Ctx.time - m_LastActionTriggerTime) >= 0.5)
+        {
+            m_LastActionTriggerTime = i_Ctx.time;
+            if (m_DialogToShow.activeInHierarchy)
+            {
+                m_DialogToShow.SetActive(false);
+            }
+            else
+            {
+                m_DialogToShow.SetActive(true);
+                m_DialogText.text = m_DialogContext;
+            }
+        }
+    }
+    
+    private void OnEnable()
+    {
+        m_InputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        m_InputActions.Disable();
     }
 }
