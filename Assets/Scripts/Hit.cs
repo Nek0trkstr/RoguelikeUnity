e8d4cb3de146924f95ee4c302f8f9e7002d45b72
Michael Shebeko <michael.shebeko@hcl.com>
2020-04-14 21:56:27 +0300

    Fix issue when enemyes can attack each over

--- Assets/Scripts/Hit.cs
+++ Assets/Scripts/Hit.cs
@@ -1,16 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Hit : MonoBehaviour
 {
     public FloatValue m_Damage;
+    private bool m_IsPlayer;
+
+    private void Awake()
+    {
+        Player m_PlayerComponent = GetComponentInParent<Player>();
+        m_IsPlayer = m_PlayerComponent ? true : false;
+    }
+
     void OnTriggerEnter2D(Collider2D i_Other)
     {
         if (i_Other.CompareTag("Hurtbox"))
         {
             Hurtbox hurtbox = i_Other.GetComponent<Hurtbox>();
-            if (hurtbox != null)
+            if (hurtbox != null && hurtbox.IsPlayer != m_IsPlayer)
             {
                 hurtbox.ReceiveDamage(m_Damage.m_RuntimeValue);
             }
--- Assets/Scripts/Sign.cs
+++ Assets/Scripts/Sign.cs
@@ -3,14 +3,14 @@ using UnityEngine.UI;
 
 public class Sign : MonoBehaviour, IInteractive
 {
-    public GameObject m_DialogToShow;
-    public Text m_DialogText;
-    public string m_DialogContext;
     public GameEvent m_SignInteractedEvent;
     public GameEvent m_SignCloseEvent;
-
+    public StringValue m_DialodueString;
+    public string m_MessageToShow;
+    
     public void Interact()
     {
+        m_DialodueString.m_RuntimeValue = m_MessageToShow;
         m_SignInteractedEvent.Raise();
     }
 
